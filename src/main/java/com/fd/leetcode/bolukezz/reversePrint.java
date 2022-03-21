5c0561ab58640c2292f20c3c9eea34c8f205e57c
zhuyumeng <1774513779@qq.com>
2022-03-21 10:18:40 +0800

    feature: submit reversePrint answer

---
+++ src/main/java/com/fd/leetcode/bolukezz/reversePrint.java
@@ -0,0 +1,25 @@
+package com.fd.leetcode.bolukezz;
+
+import com.fd.leetcode.common.ListNode;
+
+import java.util.LinkedList;
+
+/**
+ * @author zhuyumeng
+ * @date 2022/3/21 10:06 上午
+ */
+public class reversePrint {
+    public int[] reversePrint(ListNode head) {
+        LinkedList<Integer> stack = new LinkedList<Integer>();
+        while (head != null) {
+            stack.addLast(head.val);
+            head = head.next;
+        }
+        int len = stack.size();
+        int[] res = new int[len];
+        for (int i = 0; i < len; i++) {
+            res[i] = stack.pop();
+        }
+        return res;
+    }
+}
---
+++ src/main/java/com/fd/leetcode/common/ListNode.java
@@ -0,0 +1,14 @@
+package com.fd.leetcode.common;
+
+/**
+ * @author zhuyumeng
+ * @date 2022/3/21 10:06 上午
+ */
+public class ListNode {
+    public int val;
+    public ListNode next;
+
+    ListNode(int x) {
+        val = x;
+    }
+}
