90b9a2fd0104c7477758b66e17d9c217201b9051
彭摆鱼 <zhoudj100100@163.com>
2022-03-21 14:24:00 +0800

    2022.3.21

---
+++ src/main/java/com/fd/leetcode/hi100100/reversePrint.java
@@ -0,0 +1,26 @@
+package com.fd.leetcode.hi100100;
+
+import com.fd.leetcode.common.ListNode;
+
+import java.util.Stack;
+
+/**
+ * @author Administrator
+ * @data 2022/3/21  11:27
+ */
+public class reversePrint {
+    public int[] reversePrint(ListNode head) {
+        Stack<ListNode> stack = new Stack<>();
+        ListNode temp = head;
+        while (temp != null){
+            stack.add(temp);
+            temp = temp.next;
+        }
+        int size = stack.size();
+        int[] val = new int[size];
+        for (int i = 0; i < size; i++) {
+            val[i] = stack.pop().val;
+        }
+        return val;
+    }
+}
