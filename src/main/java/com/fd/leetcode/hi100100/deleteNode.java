d6f5adcbee3cafa01c3215ddd9fcde0bfa7570a1
彭摆鱼 <zhoudj100100@163.com>
2022-03-28 11:02:42 +0800

    2022.3.28 删除链表

---
+++ src/main/java/com/fd/leetcode/hi100100/deleteNode.java
@@ -0,0 +1,23 @@
+package com.fd.leetcode.hi100100;
+
+import com.fd.leetcode.common.ListNode;
+
+/**
+ * @author Administrator
+ * @data 2022/3/28  10:17
+ */
+public class deleteNode {
+    public ListNode deleteNode(ListNode head, int val) {
+        if (head == null) return null;
+        if (head.val == val) return head.next;
+        ListNode cur = head;
+        //查找到被删除元素
+        while (cur.next != null && cur.next.val != val){
+            cur = cur.next;
+        }
+        //删除元素
+        if (cur.next != null)
+            cur.next = cur.next.next;
+        return head;
+    }
+}
