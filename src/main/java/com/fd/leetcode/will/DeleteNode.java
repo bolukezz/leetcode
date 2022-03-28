71f51c0ac7423094ad22cd8dddd2bc69054becc4
will <885248661@qq.com>
2022-03-28 23:09:48 +0800

    剑指 Offer 18. 删除链表的节点

---
+++ src/main/java/com/fd/leetcode/will/DeleteNode.java
@@ -0,0 +1,23 @@
+package com.fd.leetcode.will;
+
+/**
+ * @author will
+ * @date 2022-03-28 23:08
+ */
+public class DeleteNode {
+    public ListNode deleteNode(ListNode head, int val) {
+        ListNode pre = new ListNode(0);
+        pre.next = head;
+        ListNode slow = pre;
+        ListNode fast = head;
+
+        while (fast != null) {
+            if (fast.val == val) {
+                slow.next = fast.next;
+            }
+            slow = slow.next;
+            fast = fast.next;
+        }
+        return pre.next;
+    }
+}
