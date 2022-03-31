1009cf209d0f724dfe9da0930dc9215d20da32b9
彭摆鱼 <zhoudj100100@163.com>
2022-03-31 16:10:16 +0800

    2022.3.31

---
+++ src/main/java/com/fd/leetcode/hi100100/reverseList.java
@@ -0,0 +1,39 @@
+package main.java.com.fd.leetcode.hi100100;
+
+import com.fd.leetcode.common.ListNode;
+
+/**
+ * @author zhoudj
+ * @data 2022/3/31  16:05
+ */
+public class reverseList {
+    public static ListNode reverseList(ListNode head) {
+        ListNode cur = head, pre = null;
+        while(cur != null) {
+            ListNode tmp = cur.next; // 暂存后继节点 cur.next
+            cur.next = pre;          // 修改 next 引用指向
+            pre = cur;               // pre 暂存 cur
+            cur = tmp;               // cur 访问下一节点
+        }
+        return pre;
+    }
+
+
+    public static void main(String[] args) {
+        ListNode a1 = new ListNode(1);
+        ListNode a2 = new ListNode(2);
+        ListNode a3 = new ListNode(3);
+        ListNode a4 = new ListNode(4);
+        ListNode a5 = new ListNode(5);
+        ListNode a6 = new ListNode(6);
+        a1.next = a2;
+        a2.next = a3;
+        a3.next = a4;
+        a4.next = a5;
+        a5.next = a6;
+
+        ListNode kthFromEnd = reverseList(a1);
+        System.out.println(kthFromEnd.val);
+
+    }
+}
