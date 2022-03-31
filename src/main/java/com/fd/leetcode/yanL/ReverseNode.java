2b32e7256c09e3f5220b290f796fe354fc3daa86
yanlei <1270054805@qq.com>
2022-03-31 21:57:48 +0800

    feat: ReverseNode solution

---
+++ src/main/java/com/fd/leetcode/yanL/ReverseNode.java
@@ -0,0 +1,24 @@
+package main.java.com.fd.leetcode.yanL;
+
+import com.fd.leetcode.common.ListNode;
+
+public class ReverseNode {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static ListNode solution(ListNode head) {
+        if (head == null) return null;
+        ListNode pre = null;
+        ListNode cur = head;
+        if (cur != null) {
+            ListNode next = cur.next;
+            cur.next = pre;
+            pre = cur;
+            cur = next;
+        }
+        return pre;
+    }
+
+}
