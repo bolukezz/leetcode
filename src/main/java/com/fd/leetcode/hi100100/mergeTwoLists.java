9e58c2d8c9da400081d870db9c402303051e60d3
彭摆鱼 <zhoudj100100@163.com>
2022-04-01 16:35:33 +0800

    2022.4.1

---
+++ src/main/java/com/fd/leetcode/hi100100/mergeTwoLists.java
@@ -0,0 +1,26 @@
+package main.java.com.fd.leetcode.hi100100;
+
+import com.fd.leetcode.common.ListNode;
+
+/**
+ * @author zhoudj
+ * @data 2022/4/1  15:52
+ */
+public class mergeTwoLists {
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode dum = new ListNode(0), cur = dum;
+        while(l1 != null && l2 != null) {
+            if(l1.val < l2.val) {
+                cur.next = l1;
+                l1 = l1.next;
+            }
+            else {
+                cur.next = l2;
+                l2 = l2.next;
+            }
+            cur = cur.next;
+        }
+        cur.next = l1 != null ? l1 : l2;
+        return dum.next;
+    }
+}
