898283290d90c9c215303c77e3dee37b0713516c
yanlei <1270054805@qq.com>
2022-04-02 07:52:36 +0800

    feat: MergeNode solution

---
+++ src/main/java/com/fd/leetcode/yanL/MergeNode.java
@@ -0,0 +1,40 @@
+package main.java.com.fd.leetcode.yanL;
+
+import com.fd.leetcode.common.ListNode;
+
+public class MergeNode {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static ListNode solution(ListNode l1,ListNode l2){
+        ListNode head = new ListNode(0);
+        ListNode cur = head;
+        ListNode temp;
+        while (l1 != null && l2 != null) {
+            if (l1.val > l2.val) {
+                temp = new ListNode(l2.val);
+                l2 = l2.next;
+            } else {
+                temp = new ListNode(l1.val);
+                l1 = l1.next;
+            }
+            cur.next = temp;
+            cur = cur.next;
+        }
+        while (l1 != null) {
+            temp = new ListNode(l1.val);
+            l1 = l1.next;
+            cur.next = temp;
+            cur = cur.next;
+        }
+        while (l2 != null) {
+            temp = new ListNode(l2.val);
+            l2 = l2.next;
+            cur.next = temp;
+            cur = cur.next;
+        }
+        return head.next;
+    }
+}
