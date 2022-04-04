8b080ee2b609b475848d8e4416207a52c9e40adc
litiewang <zhoudj100100@163.com>
2022-04-04 21:02:01 +0800

    2022.4.4

---
+++ src/main/java/com/fd/leetcode/hi100100/spiralOrder.java
@@ -0,0 +1,31 @@
+package main.java.com.fd.leetcode.hi100100;
+
+/**
+ * @author zhoudj
+ * @data 2022/4/4  21:01
+ */
+public class spiralOrder {
+    public int[] spiralOrder(int[][] matrix) {
+        if (matrix == null || matrix.length == 0 || matrix[0].length == 0) {
+            return new int[0];
+        }
+        int rows = matrix.length, columns = matrix[0].length;
+        boolean[][] visited = new boolean[rows][columns];
+        int total = rows * columns;
+        int[] order = new int[total];
+        int row = 0, column = 0;
+        int[][] directions = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+        int directionIndex = 0;
+        for (int i = 0; i < total; i++) {
+            order[i] = matrix[row][column];
+            visited[row][column] = true;
+            int nextRow = row + directions[directionIndex][0], nextColumn = column + directions[directionIndex][1];
+            if (nextRow < 0 || nextRow >= rows || nextColumn < 0 || nextColumn >= columns || visited[nextRow][nextColumn]) {
+                directionIndex = (directionIndex + 1) % 4;
+            }
+            row += directions[directionIndex][0];
+            column += directions[directionIndex][1];
+        }
+        return order;
+    }
+}
