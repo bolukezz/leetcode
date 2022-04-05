dba11f0dba3ef0b3b5f3476cf1a48cd8da676083
litiewang <zhoudj100100@163.com>
2022-04-05 20:13:59 +0800

    2022.4.5

---
+++ src/main/java/com/fd/leetcode/hi100100/MinStack.java
@@ -0,0 +1,30 @@
+package main.java.com.fd.leetcode.hi100100;
+
+import java.util.Stack;
+
+/**
+ * @author zhoudj
+ * @data 2022/4/5  20:13
+ */
+public class MinStack {
+    Stack<Integer> A, B;
+    public MinStack() {
+        A = new Stack<>();
+        B = new Stack<>();
+    }
+    public void push(int x) {
+        A.add(x);
+        if(B.empty() || B.peek() >= x)
+            B.add(x);
+    }
+    public void pop() {
+        if(A.pop().equals(B.peek()))
+            B.pop();
+    }
+    public int top() {
+        return A.peek();
+    }
+    public int min() {
+        return B.peek();
+    }
+}
