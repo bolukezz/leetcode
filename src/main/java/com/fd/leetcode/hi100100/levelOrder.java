86f2976e154b273bd9ffee2832c1f7998c92665e
zhoudj <zhoudj100100@163.com>
2022-04-06 16:15:50 +0800

    2022.4.6

---
+++ src/main/java/com/fd/leetcode/hi100100/levelOrder.java
@@ -0,0 +1,31 @@
+package main.java.com.fd.leetcode.hi100100;
+
+import main.java.com.fd.leetcode.common.TreeNode;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+/**
+ * @author zhoudj
+ * @data 2022/4/6  15:36
+ */
+public class levelOrder {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        Queue<TreeNode> queue = new LinkedList<>();
+        List<List<Integer>> res = new ArrayList<>();
+        if(root != null) queue.add(root);
+        while(!queue.isEmpty()) {
+            List<Integer> tmp = new ArrayList<>();
+            for(int i = queue.size(); i > 0; i--) {
+                TreeNode node = queue.poll();
+                tmp.add(node.val);
+                if(node.left != null) queue.add(node.left);
+                if(node.right != null) queue.add(node.right);
+            }
+            res.add(tmp);
+        }
+        return res;
+    }
+}
