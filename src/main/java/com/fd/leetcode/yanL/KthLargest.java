472848fec56c6102012a656556249ae0c0a677e2
yanlei <1270054805@qq.com>
2022-04-15 03:51:01 +0800

    feat: KthLargest solution

---
+++ src/main/java/com/fd/leetcode/yanL/KthLargest.java
@@ -0,0 +1,21 @@
+package main.java.com.fd.leetcode.yanL;
+
+import main.java.com.fd.leetcode.common.TreeNode;
+
+public class KthLargest {
+    int res, k;
+
+    public int kthLargest(TreeNode root, int k) {
+        this.k = k;
+        dfs(root);
+        return res;
+    }
+
+    void dfs(TreeNode root) {
+        if (root == null) return;
+        dfs(root.right);
+        if (k == 0) return;
+        if (--k == 0) res = root.val;
+        dfs(root.left);
+    }
+}
