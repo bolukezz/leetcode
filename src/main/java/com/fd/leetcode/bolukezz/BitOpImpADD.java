3678e598d4040fbc263598204dcb94eb5d987dd3
zhuyumeng <1774513779@qq.com>
2022-04-23 19:58:18 +0800

    feature: submit add  answer

---
+++ src/main/java/com/fd/leetcode/bolukezz/BitOpImpADD.java
@@ -0,0 +1,18 @@
+package main.java.com.fd.leetcode.bolukezz;
+
+
+/**
+ * @author zhuyumeng
+ * @date 2022/4/23 4:42 下午
+ */
+public class BitOpImpADD {
+    public int add(int a, int b) {
+        int carry;
+        while(b != 0) {
+            a = a^b;
+            b = (a & b) << 1;
+            a = c;
+        }
+        return a;
+    }
+}
