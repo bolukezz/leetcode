06761392f3c437e32a7003af033baaac09a93539
zhoudj <zhoudj100100@163.com>
2022-04-25 16:37:58 +0800

    2022.4.25

---
+++ src/main/java/com/fd/leetcode/hi100100/findNumberIn2DArray.java
@@ -0,0 +1,22 @@
+package main.java.com.fd.leetcode.hi100100;
+
+/**
+ * @author zhoudj
+ * @data 2022/4/25  16:36
+ */
+public class findNumberIn2DArray {
+    public boolean findNumberIn2DArray(int[][] matrix, int target) {
+        if (matrix == null || matrix.length == 0 || matrix[0].length == 0) {
+            return false;
+        }
+        int rows = matrix.length, columns = matrix[0].length;
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < columns; j++) {
+                if (matrix[i][j] == target) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
