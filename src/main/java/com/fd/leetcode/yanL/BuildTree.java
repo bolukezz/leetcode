236aa316cefcbf46b790b6d3192bcc09a99e2519
yanlei <1270054805@qq.com>
2022-04-27 02:17:31 +0800

    feat:BuildTree solution

---
+++ src/main/java/com/fd/leetcode/yanL/BuildTree.java
@@ -0,0 +1,27 @@
+package main.java.com.fd.leetcode.yanL;
+
+import main.java.com.fd.leetcode.common.TreeNode;
+
+import java.util.HashMap;
+
+public class BuildTree {
+    int[] preorder;
+    HashMap<Integer, Integer> dic = new HashMap<>();
+
+    public TreeNode solution(int[] preorder, int[] inorder) {
+        this.preorder = preorder;
+        for (int i = 0; i < inorder.length; i++)
+            dic.put(inorder[i], i);
+        return recur(0, 0, inorder.length - 1);
+    }
+
+    TreeNode recur(int root, int left, int right) {
+        if (left > right) return null;
+        TreeNode node = new TreeNode(preorder[root]);
+        int i = dic.get(preorder[root]);
+        node.left = recur(root + 1, left, i - 1);
+        node.right = recur(root + i - left + 1, i + 1, right);
+        return node;
+    }
+
+}
