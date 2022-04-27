42b253ac897970073b53b49ef9de67df04f7e487
will <885248661@qq.com>
2022-04-27 22:27:24 +0800

    剑指 Offer 12. 矩阵中的路径

---
+++ src/main/java/com/fd/leetcode/will/PathInMatrix.java
@@ -0,0 +1,35 @@
+package com.fd.leetcode.will;
+
+/**
+ * @author will
+ * @date 2022-04-27 22:26
+ */
+public class PathInMatrix {
+    public boolean exist(char[][] board, String word) {
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[0].length; j++) {
+                if (dfs(board, word.toCharArray(), i, j, 0)) return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean dfs(char[][] board, char[] word, int i, int j, int k) {
+        if (i < 0 || i >= board.length || j < 0 || j >= board[0].length || board[i][j] != word[k]) {
+            return false;
+        }
+        if (k == word.length - 1) return true;
+
+        board[i][j] = '\0';
+
+        boolean res =
+                dfs(board, word, i - 1, j, k + 1) ||
+                        dfs(board, word, i, j - 1, k + 1) ||
+                        dfs(board, word, i + 1, j, k + 1) ||
+                        dfs(board, word, i, j + 1, k + 1);
+
+        board[i][j] = word[k];
+
+        return res;
+    }
+}
